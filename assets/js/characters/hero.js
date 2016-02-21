5a202f1525553a1a69d7f46725b6ce82906ffc70
Kevin Gifford <ksgifford@gmail.com>
2016-02-21 14:45:02 -0800

    Added scripts to create SQL table on page load and populate from sample JSON file.

--- assets/js/characters/hero.js
+++ assets/js/characters/hero.js
@@ -1,98 +1,109 @@
 (function(module) {
 
-   var hero = {};
+  function Hero(opts) {
+    Object.keys(opts).forEach(function(e, index, keys) {
+      this[e] = opts[e];
+    },this);
+  };
 
-   Hero.all = [];
+  // var hero = {};
 
-   function Hero(opts) {
-     Object.keys(opts).forEach(function(e, index, keys) {
-         this[e] = opts[e];
-       },this);
-     };
+  Hero.all = [];
 
   //Create working SQL table to hold data for our heroes.
-   Hero.createTable = function() {
-     webDB.execute(
-       'CREATE TABLE IF NOT EXISTS heroes (' +
-       'id INTEGER PRIMARY KEY, ' +
-       'charName VARCHAR(40) NOT NULL, ' +
-       'created DATETIME, ' +
-       'strength INTEGER, ' +
-       'speed INTEGER, ' +
-       'stamina INTEGER, ' +
-       'achievements ARRAY, ' +
-       'users ARRAY);',
-       function(result) {
-         console.log('Successfully set up heroes table.', result);
-         if (callback) callback();
-       }
-     );
-   };
+  Hero.createTable = function() {
+    webDB.execute(
+      'CREATE TABLE IF NOT EXISTS Heroes (' +
+      'id INTEGER PRIMARY KEY, ' +
+      'charName VARCHAR(40) NOT NULL, ' +
+      'created DATETIME, ' +
+      'strength INTEGER, ' +
+      'speed INTEGER, ' +
+      'stamina INTEGER, ' +
+      'achievements ARRAY, ' +
+      'users ARRAY);',
+      function(result) {
+        console.log('Successfully set up Heroes table.', result);
+        // if (callback) callback();
+      }
+    );
+  };
 
-   Hero.clearTable = function(callback) {
-     webDB.execute(
-       'DELETE FROM heroes;',
-       callback
-     );
-   };
+  //Inserts new heroes into the SQL table .
+  Hero.prototype.populateHeroes = function(callback) {
+    webDB.execute(
+      [
+        {
+          'sql': 'INSERT INTO Heroes (charName, created, strength, speed, stamina, achievements, users) VALUES (?, ?, ?, ?, ?, ?, ?);',
+          'data': [this.charName, this.created, this.strength, this.speed, this.stamina, this.achievements, this.users],
+        }
+      ],
+      callback
+    );
+  };
 
-   //Inserts new heroes into the SQL table .
-   Hero.prototype.saveHero = function() {
-     webDB.execute(
-       [
-         {
-           'sql': 'INSERT INTO heroes (name, created, strength, speed, stamina, achievements, users) VALUES (?, ?, ?, ?, ?, ?, ?);',
-           'data': [this.charName, this.created, this.strength, this.speed, this.stamina, this.achievements, this.users],
-         }
-       ],
-     );
-   };
+  //Methods for updating data in the SQL table
+  Hero.updateRecord = function(update) {
+    webDB.execute(
+      [
+        {
+          'sql': 'UPDATE Heroes SET strength = ?, speed = ?, stamina = ? WHERE charName = ?;',
+          'data': [update.strength, update.speed, update.stamina, update.charName]
+        }
+      ]
+    );
+  };
 
-   Hero.prototype.deleteHero = function() {
-     webDB.execute(
-       [
-         {
-           'sql': 'DELETE FROM heroes WHERE id = ?;',
-           'data': [this.id]
-         }
-       ]
-     );
-   };
+  Hero.addToTable = function(e) {
+    webDB.execute(
+      [
+        {
+          'sql': 'INSERT INTO Heroes (charName, created, strength, speed, stamina, achievements, users) VALUES (?, ?, ?, ?, ?, ?, ?);',
+          'data': [e.charName, e.created, e.strength, e.speed, e.stamina, e.achievements, e.users],
+        }
+      ]
+    );
+  };
 
-   //Primary method for editing existing heroes. Every time we retrieve updated
-   //data from the API, we should call this function to update the stats for each hero.
-   Hero.prototype.updateRecord = function() {
-     webDB.execute(
-       [
-         {
-           'sql': 'UPDATE heroes SET strength = ?, speed = ?, stamina = ? WHERE name = ?;',
-           'data': [this.strength, this.speed, this.stamina, this.name]
-         }
-       ]
-     );
-   };
+  Hero.deleteFromTable = function(name) {
+    webDB.execute(
+      [
+        {
+          'sql': 'DELETE FROM Heroes WHERE charName = ?;',
+          'data': [name]
+        }
+      ]
+    );
+  };
 
-   Hero.loadAll(rows) {
-     Hero.all = rows.map(function(ele) {
-       return new Hero(ele);
-     });
-   };
+  Hero.clearTable = function(callback) {
+    webDB.execute(
+      'DELETE FROM Heroes;',
+      callback
+    );
+  };
 
-   //Pull static sample heroes from our JSON file and inserts them into the SQL table.
-   Hero.fetchHeroes = function(next) {
-     webDB.execute('SELECT * FROM heroes ORDER BY charName ASC', function(rows) {
-       $.getJSON('charModel.json', function(rawData) {
-         rawData.forEach(function(item) {
-           var hero = new Hero(item);
-           Hero.saveHero();
-         });
-         webDB.execute('SELECT * FROM heroes', function(rows) {
-           Hero.loadAll(rows);
-           next();
-         });
-       });
-     });
-   };
+  //Pull static sample heroes from our JSON file, generate an array of Hero objects,
+  // and insert them into the SQL table.
+  Hero.loadAll = function(rows) {
+    Hero.all = rows.map(function(ele) {
+      return new Hero(ele);
+    });
+  };
 
-  module.hero = hero;
+  Hero.fetchHeroes = function() {
+    webDB.execute('SELECT * FROM Heroes ORDER BY charName ASC', function(rows) {
+      $.getJSON('assets/js/characters/charModel.json', function(rawData) {
+        rawData.forEach(function(item) {
+          var hero = new Hero(item);
+          hero.populateHeroes();
+        });
+        webDB.execute('SELECT * FROM Heroes', function(rows) {
+          Hero.loadAll(rows);
+        });
+      });
+    });
+  };
+
+  module.Hero = Hero;
 })(window);

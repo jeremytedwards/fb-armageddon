83152ea2007d82e16c8dd5d5bcbf4ec4d57060b2
Predrag Josifovic <pjosifovic@yahoo.com>
2016-02-23 18:18:03 -0800

    added return to npm module that fixed the persist function

--- server.js
+++ server.js
@@ -5,54 +5,79 @@ var express = require('express'),
     port    = process.env.PORT || 3000,
     app     = express();
 
-function persistToken( token, cb ) {
-    fs.writeFile( 'fb-token.json', JSON.stringify( token ), cb );
-}
+var Fitbit  = require( 'fitbit-oauth2' );
 
-// Instantiate a fitbit client.  See example config below.
-var fitbit = new Fitbit( config.fitbit, persistToken );
+// Simple token persist functions.
+//
+var tfile = 'fb-token.json';
+var persist = {
+    read: function( filename, cb ) {
+        fs.readFile( filename, { encoding: 'utf8', flag: 'r' }, function( err, data ) {
+            if ( err ) return cb( err );
+            try {
+                var token = JSON.parse( data );
+                cb( null, token );
+            } catch( err ) {
+                cb( err );
+            }
+        });
+    },
+    write: function( filename, token, cb ) {
+        console.log( 'persisting new token:', JSON.stringify( token ) );
+        fs.writeFile( filename, JSON.stringify( token ), cb );
+    }
+};
 
+// Instanciate a fitbit client.  See example config below.
+//
+var fitbit = new Fitbit( config.fitbit );
 
-app.get( '/fitbit', function( req, res ) {
-    // if ( fitbit.getToken() )
-    //     res.redirect( '/fitbit/auth' );
-    // else  // not sure if this above is working
+// In a browser, http://localhost:4000/fitbit to authorize a user for the first time.
+//
+app.get('/fitbit', function (req, res) {
     res.redirect( fitbit.authorizeURL() );
 });
 
-
 // Callback service parsing the authorization token and asking for the access token.  This
-// endpoint is referred to in config.fitbit.authorization_uri.redirect_uri.  See example
+// endpoint is refered to in config.fitbit.authorization_uri.redirect_uri.  See example
 // config below.
-app.get( '/fitbit/auth', function( req, res, next ) {
+//
+app.get('/fitbit/auth', function (req, res, next) {
     var code = req.query.code;
     fitbit.fetchToken( code, function( err, token ) {
         if ( err ) return next( err );
-  res.redirect( '/fb-profile' );
+
+        // persist the token
+        persist.write( tfile, token, function( err ) {
+            if ( err ) return next( err );
+            res.redirect( '/fb-profile' );
+        });
     });
 });
 
-
-// fitbit.request() can be done outside of a web app context, so
-// long as a token is available and fitbit.setToken( token ) was
-// called.  fitbit.request() will automatically refresh the token
-// when required.
+// Call an API.  fitbit.request() mimics nodejs request() library, automatically
+// adding the required oauth2 headers.  The callback is a bit different, called
+// with ( err, body, token ).  If token is non-null, this means a refresh has happened
+// and you should persist the new token.
+//
 app.get( '/fb-profile', function( req, res, next ) {
-  fitbit.request({
-    uri: "https://api.fitbit.com/1/user/-/profile.json",
-method: 'GET',
-  }, function( err, body ) {
-      if ( err ) return next( err );
-var profile = JSON.parse( body );
-// res.jsonp( profile );  --> to get json!
-res.send( '<pre>' + JSON.stringify( profile, null, 2 ) + '</pre>' );
-  });
+    fitbit.request({
+        uri: "https://api.fitbit.com/1/user/-/profile.json",
+        method: 'GET',
+    }, function( err, body, token ) {
+        if ( err ) return next( err );
+        var profile = JSON.parse( body );
+        // if token is not null, a refesh has happened and we need to persist the new token
+        if ( token )
+            persist.write( tfile, token, function( err ) {
+                if ( err ) return next( err );
+                    res.send( '<pre>' + JSON.stringify( profile, null, 2 ) + '</pre>' );
+            });
+        else
+            res.send( '<pre>' + JSON.stringify( profile, null, 2 ) + '</pre>' );
+    });
 });
 
-// app.get('/fb-logout', del);
-app.get('/fb-logout',function(req, res){
-    res.redirect('http://www.fitbit.com/logout');
-  });
 
 app.use(express.static('./'));
 

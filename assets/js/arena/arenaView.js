de809f23d450f11ab86f3d45052ee2c4a81445ef
Jeremy Edwards <jeremytedwards@gmail.com>
2016-02-25 04:04:32 -0800

    IndexController revised to just hide hero and arena index.html updated to support arena states deleted arena controller from /arena, already created in routes

--- assets/js/arena/arenaView.js
+++ assets/js/arena/arenaView.js
@@ -1,48 +1,58 @@
 (function(module) {
     var arenaView = {};
 
-    arenaView.index = function() {
-        console.log('arenaView.index called...')
-        // load navigation
-
-        // hide hero and show arena section
-    };
-
-    arenaView.heroes = [];
-    arenaView.victor = {};
-
-    arenaView.hideArena = function() {
-      $("#arena").hide();
+    function getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min)) + min;
     }
 
     arenaView.battle = function() {
-      arenaView.heroes.forEach(function(h) {
-        var s = ((h.strength + h.stamina + h.speed) / 3).toFixed(0);
-        h.level = parseInt(s);
-        console.log(h.charName + " is level " + h.level);
-      })
+        arenaView.heroes.forEach(function(h) {
+            var s = ((h.strength + h.stamina + h.speed) / 3).toFixed(0);
+            h.level = parseInt(s);
+
+            console.log(h.charName + " is level " + h.level);
+        });
+
         arenaView.heroes.sort(function(a,b) {
-          console.log(a.level);
-          console.log(b.level);
-          if(a.level > b.level) {
-            arenaView.victor = a;
-            heroView.coronate(a.charName + ' is the victor.');
-          } else if(b.level > a.level) {
-            arenaView.victor = b;
-            heroView.coronate(b.charName + ' is the victor.');
-          } else {
-            heroView.coronate('The battle was a draw.');
-          }
-      })
-      //Reset the array after each battle.
-      arenaView.heroes = [];
+            console.log('level a: ' + a.level + ' vs. level b: ' + b.level);
+
+            if(a.level > b.level) {
+                arenaView.victor = a;
+                heroView.coronate(a.charName + ' is the victor.');
+            } else if(b.level > a.level) {
+                arenaView.victor = b;
+                heroView.coronate(b.charName + ' is the victor.');
+            } else {
+                heroView.coronate('The battle was a draw.');
+            }
+        });
+
+        //Reset the array after each battle.
+        arenaView.heroes = [];
+
+        console.log('arenaView.battle called....');
     }
 
+    arenaView.addHero = function() {
+        if(!arenaView.heroes.length) {
+            console.log("Adding first hero");
+
+            arenaView.heroes.push(Hero.all[0]);
+            heroView.arenaNames(arenaView.heroes[0].charName);
+        } else if(arenaController.heroes.length = 1) {
+            console.log("Adding second hero");
+
+            arenaView.heroes.push(Hero.all[0]);
+            heroView.arenaNames(arenaView.heroes[1].charName);
+        } else {
+            $('#centerArena').html('The arena has too many combatants! Please remove one before adding new Heroes to the arena.');
+        }
+
+        console.log('arenaView.addHero called....');
+    }
 
     arenaView.coronate = function(string) {
-        $('#contestant1').hide();
-        $('#contestant2').hide();
-        $('#centerArena').hide();
+        $('#contestant').hide();
         $('#victoryName').show();
         $('#victoryName').html(string);
 
@@ -50,29 +60,29 @@
     }
 
     arenaView.setArenaLeft = function() {
+        // Update view
         $("#arena").show();
+
+        // Set data elements
         arenaView.heroes.push(Hero.all[0]);
         $('#victoryName').hide();
-        $('#centerArena').show();
-        $('#contestant1').show();
+        $('#contestant').show();
         $('#contestant1').html(Hero.all[0].charName);
 
         console.log("arenaView.setArenaLeft called...");
     }
 
     arenaView.setArenaRight = function() {
+        // Update view
         $("#arena").show();
+
+        // Set data elements
         arenaView.heroes.push(Hero.all[0]);
         $('#victoryName').hide();
-        $('#centerArena').show();
-        $('#contestant2').show();
+        $('#contestant').show();
         $('#contestant2').html(Hero.all[0].charName);
 
         console.log("arenaView.setArenaRight called...");
-    }
-
-    function getRandomInt(min, max) {
-        return Math.floor(Math.random() * (max - min)) + min;
     }
 
     arenaView.displayVid = function() {
@@ -93,6 +103,53 @@
 
         console.log("arenaView.displayVid called...");
     }
+
+
+    /**  Button Handlers **/
+
+    arenaView.initBattle = function() {
+        $('#battle-btn').on('click', function(e) {
+            e.preventDefault();
+
+            // Start battle - arenaView.battle
+            arenaView.battle();
+            arenaView.displayVid();
+
+            // Update button states
+            $('#battle-btn').hide();
+
+            console.log("battle-btn clicked...");
+        });
+    }
+
+    arenaView.initCloseArena = function() {
+        $('#closeArena-btn').on('click', function(e) {
+            e.preventDefault();
+
+            // Update button states
+            $('#arena').hide();
+
+            // Load hero page??
+
+
+            console.log("battle-btn clicked...");
+        });
+    }
+
+
+
+    /**  Page Views **/
+
+    arenaView.arenaPage = function() {
+        $('#arena').show();
+
+        // render functions
+        arenaView.initBattle();
+        arenaView.initCloseArena();
+
+        console.log("arenaView.arenaPage called...");
+    }
+
 
     module.arenaView = arenaView;
 })(window);
